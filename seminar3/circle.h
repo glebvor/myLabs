593b88a131f14a8664bf8be0de8cb23b96620737
glebvor <flyalp@mail.ru>
2024-03-07 10:20:22 +0300

    circle

--- seminar3/circle.h
+++ seminar3/circle.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <math.h>
 #include "shape2d.h"
 
 class Circle : public Shape2D{
     private:
         float radius;
+        void calculateArea() override;
+        std::string getName() override;
+    
+    public:
+        Circle(float radius);
+        void scale(float scaleFactor = 1) override;
+        void showInfo() override;
 };
